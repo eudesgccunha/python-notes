ef3b3f0e65f19fb0e80de42fae986a2e55e428cf
Eudes Cunha <96583422+eudesgccunha@users.noreply.github.com>
2024-09-10 16:25:26 -0300

    Criando arquivo api.py

---
+++ python-zero/libraries.py
@@ -0,0 +1,22 @@
+#%%
+import math # biblioteca nativa do python
+
+# Dentro das bibliotecas tem módulos, classes e funções
+
+#%%
+math.sqrt(49) # o que vem depois do nome da biblioteca e o ponto é o nome da função
+
+
+# %%
+
+# você pode importar apenas parte da biblioteca para que ela não pese o seu programa
+
+from math import pi
+
+#%%
+
+# usando alias
+import math as mt # esse mt é um alias
+import pandas as pd
+
+#%%
