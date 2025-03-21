23b73de851f4687966c1422c3fb24c732227f570
Eudes Cunha <96583422+eudesgccunha@users.noreply.github.com>
2025-03-20 21:12:44 -0300

    adding dict and funtions notebooks

---
+++ python-reviews/test.py
@@ -0,0 +1,26 @@
+#%%
+x = 10
+y = 7
+z = print(int(x) + int(y))
+
+print(z)
+
+# %%
+a = 15
+b = 4
+
+print(a // b)
+# %%
+0.5 == 1/2
+# %%
+list = ['a','b','c','d','e','f']
+print(list)
+# %%
+list[:-1]
+list[-1:]
+list[-1]
+# %%
+list[-2]
+list[:-3]
+list[-3:]
+# %%
