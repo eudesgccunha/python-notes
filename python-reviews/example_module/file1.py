40979b6d65072c56474d93ea2527f2c75c6b7768
Eudes Cunha <96583422+eudesgccunha@users.noreply.github.com>
2025-03-28 14:37:29 -0300

    Adding files from example module

---
+++ python-reviews/example_module/file1.py
@@ -0,0 +1,7 @@
+print("hello from file 1")
+print("bla bla bla")
+
+if __name__ == "__main__":
+    print("file 1 is being run directly")
+else:
+    print("file 1 is being imported")
---
+++ python-reviews/example_module/file2.py
@@ -0,0 +1,8 @@
+import file1 as f1
+
+print("hello from file 2")
+
+if __name__ == "__main__":
+    print("file 2 is being run directly")
+else:
+    print("file 2 is being imported")    
