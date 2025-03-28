14b28e550f26fb93f4ae81e3898c2c5ec9cb187a
Eudes Cunha <96583422+eudesgccunha@users.noreply.github.com>
2025-03-28 14:17:02 -0300

    Adding modules archives

---
+++ python-reviews/healthy_pack/foo.py
@@ -0,0 +1,2 @@
+def fruits(name):
+   print(f'{name} is a good fruit')
---
+++ python-reviews/salute.py
@@ -0,0 +1,9 @@
+def say_hello(name):
+    """Function to greet a person by name."""
+    return f"Hello, {name}!"
+
+fruits = {
+    "name": "apple",
+    "color": "red",
+    "taste": "sweet"
+}
